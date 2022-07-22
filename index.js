fc768433e37ac2b571b8ac06e5b8bfef5b9022b4
Munyaneza Armel <munyaarmel61@gmail.com>
2022-07-22 10:11:51 +0000

    day2

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-ReactDOM.render(<h1>my first day in React wish me luck</h1>,document.getElementById('root'))
+import React from 'react'
+
+const page=(
+  <div>
+    <h1>i like react </h1>
+    <h2>react is my favorite</h2>
+  </div>
+)
+
+
+document.getElementById('root').append(JSON.stringify(page))
 //4.BUILD REACT INFOS SITES 17:14
